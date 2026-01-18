3ee2b63860c8f0c13f209d5298f8bc874c53347a
ghenriqub <ghrb2811@gmail.com>
2026-01-18 17:41:55 +0000

    ex04 done

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,35 +6,43 @@
 /*   By: ghenriqu <ghenriqu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 16:07:54 by ghenriqu          #+#    #+#             */
-/*   Updated: 2026/01/18 17:15:28 by ghenriqu         ###   ########.fr       */
+/*   Updated: 2026/01/18 17:41:41 by ghenriqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
-#include <cstring>
 
 int	main(int argc, char **argv) {
-	std::string		line;
+	if (argc != 4) {
+		std::cerr << "Usage: ./replacer [file.txt] [occurence] [replace]" << std::endl;
+		return 1;
+	}
+
 	std::string		filename(argv[1]);
 	std::string		firstStr(argv[2]);
 	std::string		secondStr(argv[3]);
 
-	if (argc != 4 || !argv[2] || !argv[3]) {
-		std::cerr << "Usage: [file.txt] [occurence] [replace]" << std::endl;
+	if (firstStr.empty()) {
+		std::cerr << "Error: search string cannot be empty" << std::endl;
 		return 1;
 	}
+
 	std::ifstream	infile(filename.c_str());
-	if (!infile.good()) {
-		std::cout << "Error: " << std::strerror(errno) << std::endl;
+	if (!infile.is_open()) {
+		std::cerr << "Error: cannot open file '" << filename << "'" << std::endl;
 		return 1;
 	}
-	std::string		newFilename = filename.append(".replace");
+
+	std::string		newFilename = filename + ".replace";
 	std::ofstream	outfile(newFilename.c_str());
-	if (!outfile.good()) {
-		std::cout << "Error: " << std::strerror(errno) << std::endl;
+	if (!outfile.is_open()) {
+		std::cerr << "Error: cannot create file '" << newFilename << "'" << std::endl;
+		infile.close();
 		return 1;
 	}
+
+	std::string		line;
 	while (std::getline(infile, line)) {
 		std::size_t	pos = 0;
 		while ((pos = line.find(firstStr, pos)) != std::string::npos) {
@@ -44,7 +52,9 @@ int	main(int argc, char **argv) {
 		}
 		outfile << line << std::endl;
 	}
+
 	infile.close();
 	outfile.close();
+
 	return 0;
 }
